68fc5e8702853516d0e92f39d8204df84c6241ba
007tickooayush <ayush.tickoo292000@gmail.com>
2024-03-27 08:43:19 +0530

    initial mongo connection

--- server-mongo-rust/src/main.rs
+++ server-mongo-rust/src/main.rs
@@ -2,6 +2,8 @@
 // use std::env;
 
 use axum::{http::StatusCode, response::IntoResponse, routing::get, Json, Router};
+// use chrono::format;
+use mongodb::{options::{ClientOptions, ResolverConfig}, Client};
 use serde_json::json;
 
 // const DB_URL: &str =env::var("DB_URL").unwrap_or("mongodb://localhost:27017".to_string());
@@ -9,6 +11,20 @@ use serde_json::json;
 #[tokio::main]
 async fn main() {
     const PORT:i32 = 4000;
+
+    // let db_user = std::env::var("MONGO_INITDB_ROOT_USERNAME").unwrap_or("root".to_string());
+    // let db_pass = std::env::var("MONGO_INITDB_ROOT_PASSWORD").unwrap_or("root".to_string());
+    let db_host = "localhost:27020";
+    let db_name = "CourseManagement_Rust";
+    // let db_url = format!("mongodb://{}:{}@{}/{}", db_user, db_pass, db_host, db_name);
+    let db_url = format!("mongodb://{}/{}",db_host,db_name);
+
+    let options = ClientOptions::parse_with_resolver_config(db_url, ResolverConfig::cloudflare()).await.unwrap();
+
+    let mongoClient = Client::with_options(options).unwrap();
+
+    println!("Connected to MongoDB");
+
 
     let app = Router::new()
         .route("/", get(|| async { (StatusCode::OK, Json(json!({"message": "Rust API Server"}))).into_response() }))
