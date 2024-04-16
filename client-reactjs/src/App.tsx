85a8b804c5eaab00bb78693c2bfb547fbeb187e5
007tickooayush <ayush.tickoo292000@gmail.com>
2024-04-16 09:35:44 +0530

    student card created

--- client-reactjs/src/App.tsx
+++ client-reactjs/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
 	
 	return (
 		<Container>
-			<Text fontSize={'xl'}>Rust + React application for School Administration System</Text>
+			<Text fontSize={'3xl'}>Rust + React application for School Administration System</Text>
 		</Container>
 	);
 }
