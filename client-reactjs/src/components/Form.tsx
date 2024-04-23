e1775075953f8f9ba57ba10b424ada2cc1ad4bf0
007tickooayush <ayush.tickoo292000@gmail.com>
2024-04-23 23:04:39 +0530

    Creating POST API Student form

--- client-reactjs/src/components/Form.tsx
+++ client-reactjs/src/components/Form.tsx
@@ -1,44 +1,57 @@
-import React, { useState } from 'react'
+import React, { FormEvent, useEffect, useState } from 'react'
+import { StudentBodySchema } from '../utils/model';
+import { Button, Center, Container, FormControl, FormLabel, HStack, Input, Radio, RadioGroup, Select, VStack } from '@chakra-ui/react';
+import { createStudent } from '../utils/api';
+
+
 
 const Form = () => {
-	const [formData, setFormData] = useState({});
-	const handleSubmit = (e) => {
+	// const [formData, setFormData] = useState<StudentBodySchema>({ name: '', uid: '', enrolled: false });
+
+	const handleSubmit = (e: React.FormEvent) => {
 		e.preventDefault();
-		const form = e.target;
+		const form: any = e.target;
 		const formObj = new FormData(form);
-		const data = Object.fromEntries(formObj.entries());
-        
-		setFormData(data);
-		console.log('Form Data:',data)
+		const data:any = Object.fromEntries(formObj.entries());
+		
+		createStudent({...data, enrolled: data.enrolled === 'true' ? true : false}).then((res) => {
+			console.log('Response:', res);
+		}).catch((err) => {
+			console.error('Error:', err);
+		});
+
+		console.log('Form State:', data);
+
 	};
 
 	return (
 		<div>
-			<form onSubmit={handleSubmit}>
-				<label htmlFor="name">Name</label>
-				<input type="text" placeholder="enter name" name='name'/>
+			<Container>
+				<form onSubmit={(e: React.FormEvent) => handleSubmit(e)}>
+					<FormControl>
+						<FormLabel htmlFor="name">Name:</FormLabel>
+						<Input type="text" name="name" id="name" placeholder='enter your name' />
 
-				<select name="options" id="options">
-					<option value="lab">lab</option>
-					<option value="practical">practical</option>
-				</select>
+						<FormLabel htmlFor="uid">UID:</FormLabel>
+						<Input type="text" name="uid" id="uid" placeholder='enter your uid' />
 
-				<label htmlFor="mern">Mern Technology</label>
-				<input type="checkbox" name="tech-mern" id="mern" />
-				<label htmlFor="java">Java technology</label>
-				<input type="checkbox" name="tech-java" id="java" />
 
-				<label htmlFor="file">File upload</label>
-				<input type="file" name="file" id="file" />
+						<FormLabel htmlFor="enrolled">Enrolled:</FormLabel>
+						<RadioGroup name="enrolled" id="enrolled">
+							<HStack spacing={4} direction="row">
+								<Radio name="enrolled" id="enrolled" value="true"> true </Radio>
+								<Radio name="enrolled" id="enrolled" value="false"> false</Radio>
+							</HStack>
+						</RadioGroup>
+						{/* <Select name="enrolled" id="enrolled">
+							<option value="true">true</option>
+							<option value="false">false</option>
+						</Select> */}
+					</FormControl>
 
-				<button type="submit">Submit</button>
-			</form>
-			<div>
-                form data is:
-				{
-					formData.name
-				}
-			</div>
+					<Button mt={4} type='submit'>Submit</Button>
+				</form>
+			</Container>
 		</div>
 	)
 }
