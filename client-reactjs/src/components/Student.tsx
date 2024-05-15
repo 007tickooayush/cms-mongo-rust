4ba1076009324a33d4143f8e4a2927bd23c5398c
007tickooayush <ayush.tickoo292000@gmail.com>
2024-05-15 19:49:36 +0530

    creating grid for details

--- client-reactjs/src/components/Student.tsx
+++ client-reactjs/src/components/Student.tsx
@@ -2,10 +2,10 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { getStudent } from '../utils/api';
 import { StudentBodySchema } from '../utils/model';
-import { Box, Flex } from '@chakra-ui/react';
+import { Box, Flex, SimpleGrid } from '@chakra-ui/react';
 
 const User = () => {
-    const [student, setStudent] = useState<StudentBodySchema>({ name: '', uid: '', enrolled: false});
+    const [student, setStudent] = useState<StudentBodySchema>({ name: '', uid: '', enrolled: false });
     const { id, ...params }: any = useParams<string>();
 
     useEffect(() => {
@@ -17,22 +17,34 @@ const User = () => {
             console.error('Error:', err);
         });
         console.log(id);
-    },[]);
+    }, []);
 
     return (
-        <Flex justifyContent={'space-around'} alignItems={'center'} flexDirection={'column'}>
-            <Box>
-                Student id: {id}
-            </Box>
-            <Box className="">
-                Student name: {student.name}
-            </Box>
-            <Box>
-                Student UID: {student.uid}
-            </Box>
-            <Box>
-                Student enrolled: {student.enrolled ? 'Yes' : 'No'}
-            </Box>
+        <Flex justifyContent={'space-around'} alignItems={'center'} flexDirection={'column'} m={4}>
+            <SimpleGrid columns={2} spacing={4}>
+                <Box>
+                    Student UID:
+                </Box>
+                <Box>
+                    {student.uid}
+                </Box>
+            </SimpleGrid>
+            <SimpleGrid columns={2} spacing={4}>
+                <Box>
+                    Student name:
+                </Box>
+                <Box>
+                    {student.name}
+                </Box>
+            </SimpleGrid>
+            <SimpleGrid columns={2} spacing={4}>
+                <Box>
+                    Student enrolled:
+                </Box>
+                <Box>
+                    {student.enrolled ? 'Yes' : 'No'}
+                </Box>
+            </SimpleGrid>
         </Flex>
     )
 }
