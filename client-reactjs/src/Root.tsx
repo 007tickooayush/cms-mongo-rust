f1a32296f096875d0666c3d5491743f5bd6eeba8
007tickooayush <ayush.tickoo292000@gmail.com>
2024-05-20 20:54:15 +0530

    implementing pagiation

--- client-reactjs/src/Root.tsx
+++ client-reactjs/src/Root.tsx
@@ -3,12 +3,14 @@ import { StudentContext } from "./utils/context";
 import { StudentType } from "./utils/model";
 
 const Root = ({ children }: React.PropsWithChildren) => {
-    
+
     const [studentsList, setStudentsList] = useState<StudentType[]>([]);
+    const [page, setPage] = useState(1);
+    const [limit, setLimit] = useState(10);
 
     return (
         <>
-            <StudentContext.Provider value={{ studentsList, setStudentsList }}>
+            <StudentContext.Provider value={{ studentsList, setStudentsList, page, setPage, limit, setLimit }}>
                 {children}
             </StudentContext.Provider>
         </>
--- client-reactjs/src/components/StudentList.tsx
+++ client-reactjs/src/components/StudentList.tsx
@@ -1,62 +1,69 @@
-import { Table, TableContainer, Th, Thead, Tr, Td, Button } from '@chakra-ui/react';
-import { useContext, useEffect } from 'react'
+import { Table, TableContainer, Th, Thead, Tr, Td, Button, Box, Flex, Text } from '@chakra-ui/react';
+import { useContext, useEffect, useState } from 'react'
 import { StudentContextType, StudentType, StudentsRespSchema } from '../utils/model';
 import { getAllStudents } from '../utils/api';
 import { StudentContext } from '../utils/context';
 import { Link } from 'react-router-dom';
 
 const StudentList = () => {
-    const { studentsList, setStudentsList }: StudentContextType = useContext(StudentContext);
-    // handle paginations
+    const { studentsList, setStudentsList, page, setPage, limit, setLimit }: StudentContextType = useContext(StudentContext);
+
+    const [nextPageStudents, setNextPageStudents] = useState<StudentType[]>([]);
+
     useEffect(() => {
         setStudentsList([])
         console.log('App component startup render')
-        getAllStudents().then((students: StudentsRespSchema) => {
+        getAllStudents(page, limit).then((students: StudentsRespSchema) => {
             setStudentsList(students.data);
-            // console.log('Data:', students.data);
-
-            // students.data.forEach((student: StudentType) => {
-            // 	console.log('Student:', new Date(parseInt(student.createdAt.$date.$numberLong)));
-            // });
         });
-
-    }, []);
+        getAllStudents(page + 1, limit).then((students: StudentsRespSchema) => {
+            console.log('Next Page: students.data :>> ', students.data.length);
+            setNextPageStudents(students.data);
+        });
+    }, [page, limit]);
     return (
-        <TableContainer>
-            <Table>
-                <Thead>
-                    <Tr>
-                        <Th>Name</Th>
-                        <Th>UID</Th>
-                        <Th>Enrolled</Th>
-                        <Th>Actions</Th>
-                    </Tr>
-                </Thead>
-                <tbody>
-                    {
-                        studentsList.map((student: StudentType, idx: number) => {
-                            return (
-                                <Tr key={idx}>
-                                    <Td>{student.name}</Td>
-                                    <Td>{student.uid}</Td>
-                                    <Td>{student.enrolled ? 'Yes' : 'No'}</Td>
-                                    <Td>
-                                        <Link to={`/student/${student.id}`}>
-                                            <Button
-                                                colorScheme={'blue'}
-                                                variant={'ghost'}
-                                            >
-                                                View
-                                            </Button>
-                                        </Link>
-                                    </Td>
-                                </Tr>
-                            )
-                        })
-                    }
-                </tbody>
-            </Table>
-        </TableContainer>
+        <Box>
+            <Flex justifyContent={'center'} alignItems={'center'} p={2}>
+                <Button width={100} onClick={() => setPage(page - 1)} isDisabled={(page <= 1) ? true: false}>Previous</Button>
+                <Text fontWeight={'bold'} ml={4} mr={4}>{page}</Text>
+                <Button width={100} onClick={() => setPage(page + 1)} isDisabled={nextPageStudents.length == 0}>Next</Button>
+            </Flex>
+            <TableContainer>
+                <Table>
+                    <Thead>
+                        <Tr>
+                            <Th>Name</Th>
+                            <Th>UID</Th>
+                            <Th>Enrolled</Th>
+                            <Th>Actions</Th>
+                        </Tr>
+                    </Thead>
+                    <tbody>
+                        {
+                            studentsList.map((student: StudentType, idx: number) => {
+                                return (
+                                    <Tr key={idx}>
+                                        <Td>{student.name}</Td>
+                                        <Td>{student.uid}</Td>
+                                        <Td>{student.enrolled ? 'Yes' : 'No'}</Td>
+                                        <Td>
+                                            <Link to={`/student/${student.id}`}>
+                                                <Button
+                                                    colorScheme={'blue'}
+                                                    variant={'ghost'}
+                                                >
+                                                    View
+                                                </Button>
+                                            </Link>
+                                        </Td>
+                                    </Tr>
+                                )
+                            })
+                        }
+                    </tbody>
+                </Table>
+            </TableContainer>
+        </Box>
     )
 }
 
